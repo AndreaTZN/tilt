a2fe6010ed48671a923e608660878ddfec84b4b4
AlexTznDev <alex.tuysuzian91@gmail.com>
2025-05-22 09:51:31 +0200

    update

--- src/components/animation.js
+++ src/components/animation.js
@@ -122,16 +122,16 @@ window.Webflow.push(() => {
       });
     }
 
-    const navBurger = document.querySelector('.nav_burger');
-    let isMenuOpen = false;
-    navBurger.addEventListener('click', () => {
-      isMenuOpen = !isMenuOpen;
-      if (isMenuOpen) {
-        document.body.style.overflow = 'hidden'; // Empêche le scroll
-      } else {
-        document.body.style.overflow = ''; // Réactive le scroll (valeur par défaut)
-      }
-    });
+    // const navBurger = document.querySelector('.nav_burger');
+    // let isMenuOpen = false;
+    // navBurger.addEventListener('click', () => {
+    //   isMenuOpen = !isMenuOpen;
+    //   if (isMenuOpen) {
+    //     document.body.style.overflow = 'hidden';
+    //   } else {
+    //     document.body.style.overflow = '';
+    //   }
+    // });
   });
 
   // ================================================
